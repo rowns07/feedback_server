b93d6263a4ca2d3a5bc2c358d20423b078c9d740
Diego Souza <diegosouza@tradecom.com.br>
2022-05-12 20:04:45 -0300

    Componentizando utilizando o conceito SOLID

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,20 @@
-import express from 'express'
-import nodemailer from 'nodemailer';
+import express from 'express';
 import { NodeMailerAdapter } from './adapters/nodemailer-adapter/nodemailer-adapter';
-import { prisma } from './prisma';
 import { PrismaFeedBackRepository } from './repositories/prisma/prisma-feebacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feeback-use-case';
 
 export const routes = express.Router();
 
-// const transport = nodemailer.createTransport({
-//   host: "smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: "f5787722ff4c0f",
-//     pass: "a10b8ace9a074b"
-//   }
-// });
-
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const prismaFeedBackRepository = new PrismaFeedBackRepository()
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedBackRepository);
   const nodeMailerAdapter = new NodeMailerAdapter();
+
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedBackRepository,
+    nodeMailerAdapter
+  )
 
   await submitFeedbackUseCase.execute({
     type,
@@ -29,28 +22,6 @@ routes.post('/feedbacks', async (req, res) => {
     screenshot
   });
 
-  await nodeMailerAdapter.sendMail({
-    to: 'Diego Souza <contato@odiegosouza.com.br>',
-    from: 'Equipe Feedget <oi@feedget.com>',
-    subject: 'Email Marketing',
-    body: [`<div style="color:#cfcfcf; background:#2f3542; width:100%; height:100%; display:flex; flex-direction: column; align-items: center; justify-content: center;">`,
-      `<p>Tipo do feebback: ${type}</p>`,
-      `<p>Comentario: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
-
-  // await transport.sendMail({
-  //   from: 'Equipe Feedget <oi@feedget.com>',
-  //   to: 'Diego Souza <dgoheri@gmail.com>',
-  //   subject: 'Novo FeedBack',
-  //   html: [
-  //     `<div style="color:#cfcfcf; background:red; width:100%; height:100%; display:flex; flex-direction: column; align-items: center; justify-content: center;">`,
-  //     `<p>Tipo do feebback: ${type}</p>`,
-  //     `<p>Comentario: ${comment}</p>`,
-  //     `</div>`
-  //   ].join('\n')
-  // });
 
   return res.status(201).send();
 
--- src/use-cases/submit-feeback-use-case.ts
+++ src/use-cases/submit-feeback-use-case.ts
@@ -1,3 +1,4 @@
+import { MailAdapter } from "../adapters/mail-adapter";
 import { FeedBacksRepository } from "../repositories/feedbacks-repository";
 
 interface SubmitFeedbackUseCaseRequest {
@@ -8,16 +9,29 @@ interface SubmitFeedbackUseCaseRequest {
 export class SubmitFeedbackUseCase {
   constructor(
     private feedbacksRepository: FeedBacksRepository,
+    private mailAdapter: MailAdapter
   ) { }
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
 
     const { type, comment, screenshot } = request;
 
-   await this.feedbacksRepository.create({
+    await this.feedbacksRepository.create({
       type,
       comment,
       screenshot
+    })
+
+    await this.mailAdapter.sendMail({
+      to: 'Diego Souza <contato@odiegosouza.com.br>',
+      from: 'Equipe Feedget <oi@feedget.com>',
+      subject: 'Email Marketing',
+      body: [`<div style="color:#cfcfcf; background:#2f3542; width:100%; height:100%; display:flex; flex-direction: column; align-items: center; justify-content: center;">`,
+        `<p>Tipo do feebback: ${type}</p>`,
+        `<p>Comentario: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}"/>` : null,
+        `</div>`
+      ].join('\n')
     })
 
   }
